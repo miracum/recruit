2229902359bf11d70a2c17986238af20df989441
chgl <5307555+chgl@users.noreply.github.com>
2026-07-04 15:57:20 +0200

    eslint and hpm uügrade

--- src/list/server/index.js
+++ src/list/server/index.js
@@ -5,12 +5,12 @@ import helmet from "helmet";
 import pino from "pino-http";
 import modifyResponse from "node-http-proxy-json";
 import { load } from "js-yaml";
-import { readFileSync } from "fs";
+import { readFileSync } from "node:fs";
 import cors from "cors";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { legacyCreateProxyMiddleware } from "http-proxy-middleware";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 import { createJwtCheck } from "./auth.js";
 import { createAccessFilter, createPatchFilter, createDeleteFilter } from "./fhirAccessFilter.js";
@@ -63,7 +63,7 @@ if (config.tracing.enabled) {
 import express from "express";
 
 // eslint-disable-next-line import/order
-import { createServer } from "http";
+import { createServer } from "node:http";
 
 import { dePseudonymize } from "./dePseudonymizer.js";
 
@@ -117,7 +117,8 @@ logger.info(config.fhirUrl);
 const allowedResourcesToPatch = /^\/\/(?<resourceType>ResearchSubject|List)/;
 
 const proxyRequestFilter = (_pathname, req) => req.method === "GET" || req.method === "PATCH" || req.method === "DELETE";
-const proxy = legacyCreateProxyMiddleware(proxyRequestFilter, {
+const proxy = createProxyMiddleware({
+  pathFilter: proxyRequestFilter,
   target: config.fhirUrl,
   changeOrigin: false,
   pathRewrite: {
@@ -125,83 +126,85 @@ const proxy = legacyCreateProxyMiddleware(proxyRequestFilter, {
   },
   secure: config.proxy.isSecureBackend,
   xfwd: true,
-  onProxyReq(proxyReq, req, res) {
-    // the ApacheProxyAddressStrategy used by HAPI FHIR
-    // constructs the server URL from both the X-Forwarded-Host and X-Forwarded-Port
-    // since the X-Forwarded-Host created by HPM already contains the port (eg. localhost:8443)
-    // the resulting FHIR server URL would end with the port number twice (eg. https://localhost:8443:8443)
-    proxyReq.removeHeader("X-Forwarded-Port");
+  on: {
+    proxyReq(proxyReq, req, res) {
+      // the ApacheProxyAddressStrategy used by HAPI FHIR
+      // constructs the server URL from both the X-Forwarded-Host and X-Forwarded-Port
+      // since the X-Forwarded-Host created by HPM already contains the port (eg. localhost:8443)
+      // the resulting FHIR server URL would end with the port number twice (eg. https://localhost:8443:8443)
+      proxyReq.removeHeader("X-Forwarded-Port");
 
-    const proto = proxyReq.getHeader("X-Forwarded-Proto");
+      const proto = proxyReq.getHeader("X-Forwarded-Proto");
 
-    if (proto) {
-      if (proto.includes("https")) {
-        proxyReq.setHeader("X-Forwarded-Proto", "https");
-      } else {
-        proxyReq.setHeader("X-Forwarded-Proto", "http");
+      if (proto) {
+        if (proto.includes("https")) {
+          proxyReq.setHeader("X-Forwarded-Proto", "https");
+        } else {
+          proxyReq.setHeader("X-Forwarded-Proto", "http");
+        }
       }
-    }
 
-    // DELETE operations are only allowed for admins
-    if (req.method === "DELETE" && !config.auth.disabled) {
-      if (!isDeleteAllowed(req.user)) {
-        res
-          .writeHead(403, {
-            "Content-Type": "text/plain",
-          })
-          .end(`Operation unauthorized. DELETEing resources requires the "admin" role.`);
-      }
-    }
-
-    // PATCH operations are only allowed on ResearchSubject (to set the recruitment status
-    // and custom note and List resources (to mark them as retired/active)
-    if (req.method === "PATCH" && !config.auth.disabled) {
-      const match = req.url.match(allowedResourcesToPatch);
-      if (match) {
-        logger.child({ url: req.url, match }).info("PATCHing a valid resource");
-        if (!isPatchRequestAllowed(match.groups.resourceType, req.user)) {
+      // DELETE operations are only allowed for admins
+      if (req.method === "DELETE" && !config.auth.disabled) {
+        if (!isDeleteAllowed(req.user)) {
           res
             .writeHead(403, {
               "Content-Type": "text/plain",
             })
-            .end(`Operation unauthorized. Unauthorized to PATCH resources of type ${match.groups.resourceType}`);
+            .end(`Operation unauthorized. DELETEing resources requires the "admin" role.`);
         }
-      } else {
-        res
-          .writeHead(403, {
-            "Content-Type": "text/plain",
-          })
-          .end("Operation unauthorized. Attempted to patch an invalid resource type");
-      }
-    }
-  },
-  onProxyRes(proxyRes, req, res) {
-    // eslint-disable-next-line no-param-reassign
-    proxyRes.headers["Cache-Control"] = "no-store";
-    return modifyResponse(res, proxyRes, async (body) => {
-      if (!body) {
-        return body;
       }
 
-      let modifiedBody = body;
-
-      if (!config.auth.disabled) {
-        modifiedBody = filterAcessibleResources(body, req.user);
+      // PATCH operations are only allowed on ResearchSubject (to set the recruitment status
+      // and custom note and List resources (to mark them as retired/active)
+      if (req.method === "PATCH" && !config.auth.disabled) {
+        const match = req.url.match(allowedResourcesToPatch);
+        if (match) {
+          logger.child({ url: req.url, match }).info("PATCHing a valid resource");
+          if (!isPatchRequestAllowed(match.groups.resourceType, req.user)) {
+            res
+              .writeHead(403, {
+                "Content-Type": "text/plain",
+              })
+              .end(`Operation unauthorized. Unauthorized to PATCH resources of type ${match.groups.resourceType}`);
+          }
+        } else {
+          res
+            .writeHead(403, {
+              "Content-Type": "text/plain",
+            })
+            .end("Operation unauthorized. Attempted to patch an invalid resource type");
+        }
       }
+    },
+    proxyRes(proxyRes, req, res) {
+      // eslint-disable-next-line no-param-reassign
+      proxyRes.headers["Cache-Control"] = "no-store";
+      return modifyResponse(res, proxyRes, async (body) => {
+        if (!body) {
+          return body;
+        }
 
-      if (config.pseudonymization.enabled) {
-        logger.debug("De-pseudonymization is enabled.");
-        if (body.resourceType === "Patient") {
-          logger.child({ resourceId: body.id }).debug("De-pseudonymizing Patient resource");
-          try {
-            modifiedBody = await dePseudonymize(config.pseudonymization, body);
-          } catch (error) {
-            logger.child({ error }).error("De-pseudonymization failed. Returning original resource.");
+        let modifiedBody = body;
+
+        if (!config.auth.disabled) {
+          modifiedBody = filterAcessibleResources(body, req.user);
+        }
+
+        if (config.pseudonymization.enabled) {
+          logger.debug("De-pseudonymization is enabled.");
+          if (body.resourceType === "Patient") {
+            logger.child({ resourceId: body.id }).debug("De-pseudonymizing Patient resource");
+            try {
+              modifiedBody = await dePseudonymize(config.pseudonymization, body);
+            } catch (error) {
+              logger.child({ error }).error("De-pseudonymization failed. Returning original resource.");
+            }
           }
         }
-      }
-      return modifiedBody;
-    });
+        return modifiedBody;
+      });
+    },
   },
 });
 
