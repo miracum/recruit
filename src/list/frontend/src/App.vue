dd15a226b68092e2ef36db7834dd253efbe9f3fc
chgl <5307555+chgl@users.noreply.github.com>
2026-07-04 17:09:13 +0200

    feat: added theme switcher

--- src/list/frontend/src/App.vue
+++ src/list/frontend/src/App.vue
@@ -18,6 +18,10 @@
             <b-icon pack="fas" size="is-small" type="is-white" :icon="isAdmin ? 'user-cog' : 'user'"></b-icon>
             <span class="mr-3 ml-3 has-text-white">{{ username }}</span>
             <div class="buttons">
+              <b-tooltip :label="isDarkTheme ? 'Helles Design aktivieren' : 'Dunkles Design aktivieren'" position="is-left">
+                <b-button outlined size="is-small" type="is-white" :icon-left="isDarkTheme ? 'sun' : 'moon'" @click="toggleTheme">
+                </b-button>
+              </b-tooltip>
               <b-button v-if="isAuthenticated" outlined type="is-white" size="is-small" @click="logout">Ausloggen
               </b-button>
               <b-tooltip label="Benutzerhilfe öffnen" position="is-left">
@@ -45,6 +49,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      isDarkTheme: false,
+    };
+  },
   computed: {
     version: () => import.meta.env.VITE_APP_VERSION,
     username: function username() {
@@ -57,9 +66,22 @@ export default {
       return this.isAuthenticated && this.$keycloak.hasResourceRole("admin");
     },
   },
+  created() {
+    const storedTheme = localStorage.getItem("theme");
+    this.isDarkTheme = storedTheme ? storedTheme === "dark" : window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (storedTheme) {
+      document.documentElement.dataset.theme = storedTheme;
+    }
+  },
   methods: {
     logout: function logout() {
       this.$keycloak.logoutFn();
+    },
+    toggleTheme: function toggleTheme() {
+      this.isDarkTheme = !this.isDarkTheme;
+      const theme = this.isDarkTheme ? "dark" : "light";
+      document.documentElement.dataset.theme = theme;
+      localStorage.setItem("theme", theme);
     },
   },
 };
